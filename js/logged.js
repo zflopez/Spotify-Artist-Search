a944631d27a13838cd83cf07c988bb5ab762a58c
zflopez <zaida.flopez@gmail.com>
2016-06-29 17:50:43 +0200

    albums found

--- js/logged.js
+++ js/logged.js
@@ -30,12 +30,18 @@ $(document).ready(function() {
 
 function requestArtist(artistId) {
 	$.ajax({
-		url: "https://api.spotify.com/v1/artists/" + artistId + "/albums",
+		url: "https://api.spotify.com/v1/artists/" + artistId + "/albums?market=ES",
 		type: "GET",
 		dataType: "json",
 		success: function(artistAlbums) {
-			var albums = dataReceived.artists.items[0].id;
-			console.log();
+			for (var i = 0; i < artistAlbums.items.length; i++) {
+				var albums = artistAlbums.items[i].name;
+				var albumImg = artistAlbums.items[i].images[0].url;
+				var html = "<div><p><strong>" + albums + "</strong></p><p><img src='" + albumImg + "'</p></div>";
+				
+				$('body').append(html);
+				console.log(albums);
+			}			
 		},
 		error: function(response) {
 			console.log(response);
